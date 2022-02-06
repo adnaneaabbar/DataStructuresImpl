fd0b980f8fc9040c730617459822fd7aba0c20e2
adnaneaabbar <adnaneaabbar@gmail.com>
2022-02-06 18:29:30 +0100

    graph boilerplate

---
+++ graph.py
@@ -0,0 +1,26 @@
+# class Graph:
+#     def __init__(self, value):
+#
+#     def insert(self, newValue):
+#
+#     def bfs(self):
+#
+#     def dfs(self):
+#
+#     def search(self, searchValue):
+#
+#     def minimum(self):
+#
+#     def maximum(self):
+
+
+# def count(graph):
+#     if graph is None:
+#         return 0
+#     return
+
+def driver(name):
+    print(f"{name} Driver Code")
+
+if __name__ == "__main__":
+    driver("Graph")
